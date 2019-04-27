b1586262ed2eda73cd09cf2c08a268c55355e429
kazuya-hachisuka <kzy8sk@gmail.com>
2019-04-27 23:51:23 +0900

    hello.js add song

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,4 +4,10 @@ module.exports = (robot) => {
     const username = msg.message.user.name;
     msg.send('Hello, ' + username);
   });
+
+  robot.hear(/song>/i, (msg) => {
+    const username = msg.message.user.name;
+    const lyric = "Hu-bot!! \n みんなのHu-bot!! \n いつでもHu-bot!!! \n オイラはHu-bot!!! \n"
+    msg.send(lyric + username);
+  });
 };
